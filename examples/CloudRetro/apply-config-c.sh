ac8e3a52163d1d8731935b94d673689b5a5f67c1
Valniae <74309145+bbalint105@users.noreply.github.com>
2022-07-22 14:06:32 +0200

    -

--- examples/CloudRetro/apply-config-c.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Minimalistic sed usage to edit and re-apply Worker and Coordinator configMap with new env. variables, then restart deployment
-#
-kubectl get configmap -n cloudretro cloudretro-config-c -o yaml | \
-sed 's/CLOUD_GAME_WEBRTC_ICESERVERS_0_URL: NULL/CLOUD_GAME_WEBRTC_ICESERVERS_0_URL: turn:'"$(kubectl get service -n stunner stunner-gateway-udp-gateway-svc -o jsonpath='{.status.loadBalancer.ingress[0].ip}')"':3478/' | \
-sed 's/CLOUD_GAME_WEBRTC_ICESERVERS_0_URL: NULL/CLOUD_GAME_WEBRTC_ICESERVERS_0_USERNAME: '"$(kubectl get gatewayconfig -n stunner stunner-gatewayconfig -o jsonpath='{.spec.userName}')"'/' | \
-sed 's/CLOUD_GAME_WEBRTC_ICESERVERS_0_URL: NULL/CLOUD_GAME_WEBRTC_ICESERVERS_0_CREDENTIAL: '"$(kubectl get gatewayconfig -n stunner stunner-gatewayconfig -o jsonpath='{.spec.password}')"'/' | \
-kubectl apply -f -
-kubectl rollout restart deployment -n cloudretro coordinator-deployment
