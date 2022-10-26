cdd10e2a9b252731745ef6ee77a2d0ffd4d38d29
Kornel David <konel1020@gmail.com>
2022-10-26 11:13:18 +0200

    refactor: Small changes

--- cmd/performance/eval.sh
+++ cmd/performance/eval.sh
@@ -21,19 +21,19 @@ then
     UDP_ECHO_IP="127.0.0.1"
     IPERF_PORT="8999"
 else
-    STUNNER_PUBLIC_ADDR=$(kubectl get cm stunner-config -o jsonpath='{.data.STUNNER_PUBLIC_ADDR}')
-    STUNNER_PUBLIC_PORT=$(kubectl get cm stunner-config -o jsonpath='{.data.STUNNER_PUBLIC_PORT}')
-    STUNNER_REALM=$(kubectl get cm stunner-config -o jsonpath='{.data.STUNNER_REALM}')
-    STUNNER_PASSWORD=$(kubectl get cm stunner-config -o jsonpath='{.data.STUNNER_PASSWORD}')
-    STUNNER_USERNAME=$(kubectl get cm stunner-config -o jsonpath='{.data.STUNNER_USERNAME}')
-    UDP_ECHO_IP=$(kubectl get svc udp-echo -o jsonpath='{.spec.clusterIP}')
-    IPERF_PORT="9001"
+    STUNNER_PUBLIC_ADDR=$(kubectl get cm stunnerd-config -n stunner -o jsonpath='{.data.stunnerd\.conf}' | jq -r .listeners[0].public_address)
+    STUNNER_PUBLIC_PORT=$(kubectl get cm stunnerd-config -n stunner -o jsonpath='{.data.stunnerd\.conf}' | jq -r .listeners[0].public_port)
+    STUNNER_REALM=$(kubectl get cm stunnerd-config -n stunner -o jsonpath='{.data.stunnerd\.conf}' | jq -r .auth.realm)
+    STUNNER_PASSWORD=$(kubectl get cm stunnerd-config -n stunner -o jsonpath='{.data.stunnerd\.conf}' | jq -r .auth.credentials.password)
+    STUNNER_USERNAME=$(kubectl get cm stunnerd-config -n stunner -o jsonpath='{.data.stunnerd\.conf}' | jq -r .auth.credentials.username)
+    UDP_ECHO_IP=$(kubectl get svc iperf-server -o jsonpath='{.spec.clusterIP}')
+    IPERF_PORT="5000"
 fi
 
 for i in $(seq $num_of_processes); 
 do
     port=$((8999+$i))
-    go run ../turncat/main.go --realm $STUNNER_REALM --log=all:INFO udp://127.0.0.1:$port \
+    go run ../turncat/main.go --log=all:DEBUG udp://127.0.0.1:$port \
     turn://${STUNNER_USERNAME}:${STUNNER_PASSWORD}@${STUNNER_PUBLIC_ADDR}:${STUNNER_PUBLIC_PORT} udp://${UDP_ECHO_IP}:$IPERF_PORT &
 done
 
@@ -43,7 +43,8 @@ for i in $(seq $num_of_processes);
 do
     port=$((8999+$i))
     # port=8999
-    iperf -c 127.0.0.1 -u -p $port -t $eval_time -i 1 -b $bandwidth -l $packet_size &> /dev/null &
+    #iperf -c 127.0.0.1 -u -p $port -t $eval_time -i 1 -b $bandwidth -l $packet_size &> /dev/null &
+    iperf -c 127.0.0.1 -u -p $port -t $eval_time -i 1 -b $bandwidth -l $packet_size &
     # echo "hello" | socat -d -d - udp:127.0.0.1:$port
 done
 
