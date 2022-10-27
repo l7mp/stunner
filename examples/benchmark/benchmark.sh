a53a69d4448e2328393f89454397ce5f7358dc6e
Kornel David <konel1020@gmail.com>
2022-10-27 18:55:30 +0200

    refactor: Move benchmark from cmd to examples

--- examples/benchmark/benchmark.sh
+++ examples/benchmark/benchmark.sh
@@ -6,21 +6,26 @@ packet_size=$3
 bandwidth=$4
 platform=$5
 
-echo "Number of concurrent turncat processes: $num_of_processes"
-echo "Evaluation time: $eval_time s"
+echo "Number of concurrent turncat clients: $num_of_processes"
+echo "Evaluation time: $eval_time sec"
 echo "Packet size: $packet_size bytes"
+echo "Bandwidth: $(($bandwidth / 1000)) Kbits/sec or $(($bandwidth / 1000 / 1000)) Mbits/sec per turncat client"
 echo "Platform: $platform"
 
-if [[ $platform == "local" ]];
-then
+if [[ $platform == "local" ]]; then
     STUNNER_PUBLIC_ADDR="127.0.0.1"
-    STUNNER_PUBLIC_PORT="5000"
+    STUNNER_PUBLIC_PORT="5001"
     STUNNER_REALM="stunner.l7mp.io"
     STUNNER_USERNAME="user1"
     STUNNER_PASSWORD="passwd1"
     UDP_ECHO_IP="127.0.0.1"
-    IPERF_PORT="8999"
-else
+    IPERF_PORT="5000"
+
+    go run ../../cmd/stunnerd/main.go --log=all:INFO turn://${STUNNER_USERNAME}:${STUNNER_PASSWORD}@${STUNNER_PUBLIC_ADDR}:${STUNNER_PUBLIC_PORT} &> /dev/null 2>&1 &
+    iperf -s -p 5000 -u -e &
+    sleep 2
+
+elif [[ $platform == "k8s" ]]; then
     STUNNER_PUBLIC_ADDR=$(kubectl get cm stunnerd-config -n stunner -o jsonpath='{.data.stunnerd\.conf}' | jq -r .listeners[0].public_address)
     STUNNER_PUBLIC_PORT=$(kubectl get cm stunnerd-config -n stunner -o jsonpath='{.data.stunnerd\.conf}' | jq -r .listeners[0].public_port)
     STUNNER_REALM=$(kubectl get cm stunnerd-config -n stunner -o jsonpath='{.data.stunnerd\.conf}' | jq -r .auth.realm)
@@ -28,27 +33,29 @@ else
     STUNNER_USERNAME=$(kubectl get cm stunnerd-config -n stunner -o jsonpath='{.data.stunnerd\.conf}' | jq -r .auth.credentials.username)
     UDP_ECHO_IP=$(kubectl get svc iperf-server -o jsonpath='{.spec.clusterIP}')
     IPERF_PORT="5000"
+else
+    echo "Platform '$platform' is invalid, only 'local' and 'k8s' are valid"
+    exit
 fi
 
 for i in $(seq $num_of_processes); 
 do
     port=$((8999+$i))
-    go run ../turncat/main.go --log=all:DEBUG udp://127.0.0.1:$port \
-    turn://${STUNNER_USERNAME}:${STUNNER_PASSWORD}@${STUNNER_PUBLIC_ADDR}:${STUNNER_PUBLIC_PORT} udp://${UDP_ECHO_IP}:$IPERF_PORT &
+    go run ../../cmd/turncat/main.go --log=all:INFO udp://127.0.0.1:$port \
+    turn://${STUNNER_USERNAME}:${STUNNER_PASSWORD}@${STUNNER_PUBLIC_ADDR}:${STUNNER_PUBLIC_PORT} udp://${UDP_ECHO_IP}:$IPERF_PORT &> /dev/null 2>&1 &
 done
 
-sleep 3
+sleep 2
 
 for i in $(seq $num_of_processes); 
 do
     port=$((8999+$i))
-    # port=8999
-    #iperf -c 127.0.0.1 -u -p $port -t $eval_time -i 1 -b $bandwidth -l $packet_size &> /dev/null &
-    iperf -c 127.0.0.1 -u -p $port -t $eval_time -i 1 -b $bandwidth -l $packet_size &
-    # echo "hello" | socat -d -d - udp:127.0.0.1:$port
+    iperf -c 127.0.0.1 -u -p $port -t $eval_time -i 1 -b $bandwidth -l $packet_size &> /dev/null &
+    # iperf -c 127.0.0.1 -u -p $port -t $eval_time -i 1 -b $bandwidth -l $packet_size &
+
 done
 
-sleep $(($eval_time+1))
-killall main &> /dev/null
-# killall iperf
+sleep $(($eval_time+2))
+killall main -w &> /dev/null
+killall iperf -w &> /dev/null
 exit
