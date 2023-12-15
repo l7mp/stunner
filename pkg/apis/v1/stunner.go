19df3f1c0b569443b21ea0917e20297278438676
Gabor Retvari <retvari@tmit.bme.hu>
2023-12-15 18:28:27 +0100

    fix: Fix error ib API.V1 DeepEqual

--- pkg/apis/v1/stunner.go
+++ pkg/apis/v1/stunner.go
@@ -83,19 +83,20 @@ func (req *StunnerConfig) DeepEqual(conf Config) bool {
 		return false
 	}
 
+	if len(req.Listeners) != len(other.Listeners) {
+		return false
+	}
 	for i := range req.Listeners {
-		if i >= len(other.Listeners) {
-			return false
-		}
 		if !req.Listeners[i].DeepEqual(&other.Listeners[i]) {
 			return false
 		}
 	}
 
+	if len(req.Clusters) != len(other.Clusters) {
+		return false
+	}
+
 	for i := range req.Clusters {
-		if i >= len(other.Clusters) {
-			return false
-		}
 		if !req.Clusters[i].DeepEqual(&other.Clusters[i]) {
 			return false
 		}
