67f599a5bd6e37dc6d954eea34baab070a267eb9
Gabor Retvari <retvari@tmit.bme.hu>
2026-06-08 16:30:41 +0200

    Phase 4: Clean up object dir

--- internal/object/runtime.go
+++ internal/object/runtime.go
@@ -6,6 +6,7 @@ import (
 	"github.com/l7mp/stunner/internal/quota"
 	"github.com/l7mp/stunner/internal/resolver"
 	"github.com/l7mp/stunner/internal/telemetry"
+	licensecfg "github.com/l7mp/stunner/pkg/config/license"
 	"github.com/l7mp/stunner/pkg/logger"
 )
 
@@ -48,6 +49,44 @@ type Registry interface {
 	Add(o Object) error
 	// Remove unregisters an object.
 	Remove(o Object) error
+}
+
+// Router is an object that knows how to match clusters and listeners.
+type Router interface {
+	// GetAdmin returns the admin object. Panics if no admin object is available.
+	GetAdmin() *Admin
+
+	// GetAuth returns the authenitation object. Panics if no auth object is available.
+	GetAuth() *Auth
+
+	// GetListeners returns all STUNner listeners.
+	GetListeners() []*Listener
+
+	// GetListener returns a STUNner listener or nil of no listener with the given name was found.
+	GetListener(name string) *Listener
+
+	// GetClusters returns all STUNner clusters.
+	GetClusters() []*Cluster
+
+	// GetCluster returns a STUNner cluster or nil if no cluster with the given name was found.
+	GetCluster(name string) *Cluster
+
+	// GetLicenseConfigManager returns the license manager.
+	GetLicenseConfigManager() licensecfg.ConfigManager
+}
+
+// GetClustersForListener returns the clusters for a listener.
+func GetClustersForListener(l *Listener) []*Cluster {
+	ret := []*Cluster{}
+	for _, route := range l.Routes {
+		for _, o := range l.reg.LookupAll(TypeCluster) {
+			if o.ObjectName() == route {
+				ret = append(ret, o.(*Cluster))
+			}
+		}
+	}
+
+	return ret
 }
 
 // Runtime carries per-STUNner process dependencies used by object constructors.
